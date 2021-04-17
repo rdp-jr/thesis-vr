bcb765f0e798c0507ddaf9cf4269331bb7963c56
Roberto Principio Jr <rdprincipio.jr@gmail.com>
2021-04-17 21:56:20 +0800

    spawn outside

---
+++ public/static/assets/js/spawn-outside.component.js
@@ -0,0 +1,8 @@
+AFRAME.registerComponent('spawn-outside', {
+  
+    init: function() {
+      var el = this.el;
+      el.setAttribute('position', {x: -0.078, y: 1.600, z: 9.505});
+     
+    }
+  });
