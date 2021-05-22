fa9f54331cadf946d0d11ae2861842495ca220fb
rdp-jr <rdprincipio.jr@gmail.com>
2021-05-23 02:51:29 +0800

    add modified blink teleportation, position rotation correctly syncs

--- public/static/assets/js/spawn-outside.component.js
+++ public/static/assets/js/spawn-outside.component.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('spawn-outside', {
   
     init: function() {
       var el = this.el;
-      el.setAttribute('position', {x: 0.093, y: 0, z: 6.270});
+      el.setAttribute('position', {x: 0.093, y: 1.6, z: 6.270});
      
     }
   });
