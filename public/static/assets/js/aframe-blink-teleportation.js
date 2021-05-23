e34cc8ea18d8f77219d148b19b1e7a1234988b1b
rdp-jr <rdprincipio.jr@gmail.com>
2021-05-23 22:50:48 +0800

    revert modifications to blink-tel component, add networked attr to cameraRig

--- public/static/assets/js/aframe-blink-teleportation.js
+++ public/static/assets/js/aframe-blink-teleportation.js
@@ -1,8 +1,7 @@
 AFRAME.registerComponent('blink-teleportation', {
 	schema: {
         camera: {type: 'selector', default: '#cam'},
-        cameraRig: {type: 'selector', default: '.camRig'},
-        player: {type: 'selector', default: '#player'},
+        cameraRig: {type: 'selector', default: '#camRig'},
         deviceButtons: {default: ['click', 'mousedown', 'triggerdown']},
         cursor: {type: 'string', default: '#my-cursor'},
         raycasterObjects: {type: 'string', default: '.clickable'},
